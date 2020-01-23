a8254af6dab7fc76995fa6f52508baf34cf6f9e9
Dominic Tremblay <dominictremblay24@gmail.com>
2020-01-22 22:05:05 -0500

    config for demo = start here

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,12 @@ import './App.css';
 import NavBar from './NavBar';
 import ChatBar from './ChatBar';
 import MessageList from './MessageList';
-//import lib from './lib/messages';
-import dataReducer, { SET_MESSAGES, SET_USERNAME } from './reducer/dataReducer';
+import lib from './lib/messages';
 
 function App() {
-  //const [messages, setMessages] = useState(lib.messages);
-  //const [currentUser, setCurrentUser] = useState({ name: 'Anonymous' });
+  const [messages, setMessages] = useState(lib.messages);
+  const [currentUser, setCurrentUser] = useState({ name: 'Anonymous' });
 
-  const [state, dispatch] = useReducer(dataReducer, {
-    currentUser: { name: 'Anonymous' },
-    messages: [],
-  });
   // Sending message from the chat to the server
   const sendMessage = message => {
     // Create a new message object
@@ -23,11 +18,10 @@ function App() {
         .substr(2, 6),
       type: 'incomingMessage',
       content: message,
-      username: state.currentUser.name,
+      username: currentUser.name,
     };
 
-    dispatch({ type: SET_MESSAGES, message: newMessage });
-    //setMessages([...messages, newMessage]);
+    setMessages([...messages, newMessage]);
   };
 
   const updateUser = username => {
@@ -37,22 +31,20 @@ function App() {
         .toString(36)
         .substr(2, 6),
       type: 'incomingNotification',
-      content: `${state.currentUser.name} has changed their name to ${username}`,
+      content: `${currentUser.name} has changed their name to ${username}`,
     };
 
     // updating the username in the state
-    //setCurrentUser({ name: username });
-    dispatch({ type: SET_USERNAME, username });
-    //setMessages([...messages, newNotification]);
-    dispatch({ type: SET_MESSAGES, message: newNotification });
+    setCurrentUser({ name: username });
+    setMessages([...messages, newNotification]);
   };
 
   return (
     <div>
       <NavBar />
-      <MessageList messages={state.messages} />
+      <MessageList messages={messages} />
       <ChatBar
-        username={state.currentUser.name}
+        username={currentUser.name}
         sendMessage={sendMessage}
         updateUser={updateUser}
       />
